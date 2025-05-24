5b437ca6d4fd48d60bd36bd57a6dddd245190c02
coffeek <berezkov.dima.16@mail.ru>
2025-05-24 21:11:17 +0500

    Create project and add package data, domain, di, presentation

---
+++ app/src/main/java/com/example/flavi/presentation/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.flavi.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.flavi.ui.theme.FlaviTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FlaviTheme {
+
+            }
+        }
+    }
+}
