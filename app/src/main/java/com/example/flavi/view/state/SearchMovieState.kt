5a07b17a79220854a6d664a4b4a3ac737fc72394
coffeek <berezkov.dima.16@mail.ru>
2025-07-12 17:18:06 +0500

    Обновление работы с сеть часть 2.1

---
+++ app/src/main/java/com/example/flavi/view/state/SearchMovieState.kt
@@ -0,0 +1,35 @@
+package com.example.flavi.view.state
+
+import com.example.flavi.model.data.datasource.CountriesDTO
+import com.example.flavi.model.data.datasource.GenresDTO
+import com.example.flavi.model.data.datasource.PosterDTO
+import com.example.flavi.model.data.datasource.RatingDTO
+
+sealed interface SearchMovieState {
+    data object Initial : SearchMovieState
+
+    data object NotFound : SearchMovieState
+
+    data object NetworkShutdown : SearchMovieState
+
+    data class NotificationOfInternetLoss(
+        val notification: String
+    ) : SearchMovieState
+
+    data object ConnectToTheNetwork : SearchMovieState
+
+    data class InputQuery(
+        val query: String
+    ) : SearchMovieState
+
+    data class LoadMovie(
+        val id: Int,
+        val name: String,
+        val alternativeName: String,
+        val year: Int,
+        val posterDTO: PosterDTO,
+        val ratingDTO: RatingDTO,
+        val genresDTO: GenresDTO,
+        val countriesDTO: CountriesDTO
+    ) : SearchMovieState
+}
